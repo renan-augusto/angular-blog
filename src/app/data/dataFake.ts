2d465dd219232ca598f3f81fca31ee3d1c666a7c
renan-augusto <renan_acm1992@hotmail.com>
2023-07-06 06:45:52 -0300

    componentizacao final/dinamismo

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,25 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Tom Morello anuncia nova guitarra',
+    description:
+      'Saiba mais sobre o modelo lancado em parceria com a fender USA do guitarrista Tom morello.',
+    photoCover:
+      'https://www.tenhomaisdiscosqueamigos.com/wp-content/uploads/2020/04/tom-morello-fender-strato.jpg',
+  },
+  {
+    id: '2',
+    title: 'Tribuo a Eddie Van Halen',
+    description:
+      'Foi anunciado um disco de tributo a Eddie Van Halen que contara com os mais diversos guitarristas.',
+    photoCover:
+      'https://uploads.metropoles.com/wp-content/uploads/2020/10/06164657/GettyImages-490791136.jpg',
+  },
+  {
+    id: '3',
+    title: 'Slash - Saul Hudson',
+    description: 'Saiba mais sobre o guitarrista do Guns and Roses.',
+    photoCover:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGGTRQTxacyds0k68DlfA2dyouJ6-VoVGazGviRap8&',
+  },
+];
